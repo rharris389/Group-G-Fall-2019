d3aaaccdbae6ea0c4a425767c657682c06cc3369
Sion Pixley <sionpixley@gmail.com>
2019-10-28 13:13:46 -0500

    Added some views

--- API/EarlyBirdApi/Api/serializers.py
+++ API/EarlyBirdApi/Api/serializers.py
@@ -1,27 +1,17 @@
 from rest_framework import serializers
-from .models import User, Event, TimeRestriction, ConfigurationEvent, ConfigurationTimeRestriction
+from .models import User, Event, TimeRestriction
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('Id', 'Email', 'Passwd', 'Gender', 'FirstName', 'LastName')
+        fields = ('Id', 'Username', 'Email', 'Passwd', 'Gender', 'FirstName', 'LastName')
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('Id', 'Name', 'StartDate', 'EndDate', 'NotificationDate', 'IsGoal', 'Frequency')
+        fields = ('Id', 'Name', 'StartDate', 'EndDate', 'NotificationDate', 'IsGoal', 'Frequency', 'UserId')
 
 class TimeRestrictionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TimeRestriction
-        fields = ('Id', 'StartDate', 'EndDate', 'Frequency')
-
-class ConfigurationEventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ConfigurationEvent
-        fields = ('Id', 'UserId', 'EventId')
-
-class ConfigurationTimeRestriction(serializers.ModelSerializer):
-    class Meta:
-        model = ConfigurationTimeRestriction
-        fields = ('Id', 'UserId', 'TimeRestrictionId')
+        fields = ('Id', 'StartDate', 'EndDate', 'Frequency', 'UserId')
--- API/EarlyBirdApi/Api/views.py
+++ API/EarlyBirdApi/Api/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from .models import User, Event, TimeRestriction
+from .serializers import UserSerializer, EventSerializer, TimeRestrictionSerializer
 
-# Create your views here.
+@csrf_exempt
+def AddUser(request):
+    if request.method == 'POST':
+        userData = JSONParser().parse(request)
+        user = UserSerializer(data=userData)
+        if user.is_valid():
+            user.save()
+            return HttpResponse(status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def GetUser(request, username):
+    try:
+        user = User.objects.get(Username=username)
+    except User.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        userData = UserSerializer(user)
+        return JsonResponse(userData.data)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def GetPasswd(request, username):
+    try:
+        user = User.objects.get(Username=username)
+    except User.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return HttpResponse(user.Passwd)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def AddEvent(request):
+    if request.method == 'POST':
+        eventData = JSONParser().parse(request)
+        event = EventSerializer(data=eventData)
+        if event.is_valid():
+            return HttpResponse(status=status.HTTP_201_CREATED)
+        else:
+            return  HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def GetEvents(request, start, end):
+    try:
+        events = Event.objects.values.filter(StartDate__gte=start, EndDate__lt=end)
+    except Event.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return JsonResponse(events.data)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
