cc80f6a6a95f170a5d8fee27c6249bda5589ae69
rharris389 <56176404+rharris389@users.noreply.github.com>
2019-11-12 20:30:44 -0600

    Update MainActivity.java

--- Early_Bird/app/src/main/java/com/example/earlybird/MainActivity.java
+++ Early_Bird/app/src/main/java/com/example/earlybird/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.earlybird;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
